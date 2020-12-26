80f99f6976c54674d7ab35041335a25cb0b6a60c
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-12-26 20:08:26 +0200

    Initial Commit

---
+++ RealityKItWithSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RealityKItWithSwiftUI
+//
+//  Created by Ramill Ibragimov on 26.12.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RealityKItWithSwiftUI/RealityKItWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealityKItWithSwiftUIApp.swift
+//  RealityKItWithSwiftUI
+//
+//  Created by Ramill Ibragimov on 26.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct RealityKItWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
