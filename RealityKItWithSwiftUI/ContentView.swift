8c48b579b79ec701f112f2b579dd6248140ca8c1
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-12-26 20:22:06 +0200

    init

--- RealityKItWithSwiftUI/ContentView.swift
+++ RealityKItWithSwiftUI/ContentView.swift
@@ -6,11 +6,36 @@
 //
 
 import SwiftUI
+import RealityKit
+
+struct ARViewWrapper: UIViewRepresentable {
+    typealias UIViewType = ARView
+    
+    func makeUIView(context: Context) -> ARView {
+        let arView = ARView(frame: .zero, cameraMode: .ar, automaticallyConfigureSession: true)
+        
+        let mesh = MeshResource.generateBox(size: 0.2)
+        let material = SimpleMaterial(color: .blue, roughness: 0.5, isMetallic: true)
+        let modelEntity = ModelEntity(mesh: mesh, materials: [material])
+        let anchorEntity = AnchorEntity(plane: .horizontal)
+        anchorEntity.addChild(modelEntity)
+        
+        arView.scene.addAnchor(anchorEntity)
+        
+        return arView
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {
+        
+    }
+}
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            ARViewWrapper()
+            Text("Hello AR")
+        }
     }
 }
 
